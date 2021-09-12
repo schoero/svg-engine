96eccde0c550df8315b931a788fb713278bad724
Roger Schönbächler <schoero@gmx.ch>
2021-09-12 16:01:05 +0200

    Add browser test

--- test/index.js
+++
@@ -1,8 +0,0 @@
-//@ts-check
-import SVG from "../lib/node/index.js";
-
-const svg = new SVG();
-const rect = svg.addRect(10, 10, "10cm", "2cm");
-rect.fill("red");
-const text = svg.addText("Hello world!");
-console.log(svg.outerHTML);
