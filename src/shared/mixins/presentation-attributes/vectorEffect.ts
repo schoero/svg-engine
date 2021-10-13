25b9587269ef8c11de2acbdc994f3a4e8ef4eb3b
Roger Schönbächler <schoero@gmx.ch>
2021-10-13 20:48:22 +0200

    [VectorEffect] Fix vectorEffect values

--- src/shared/mixins/presentation-attributes/vectorEffect.ts
+++ src/shared/mixins/presentation-attributes/vectorEffect.ts
@@ -3,8 +3,8 @@ import { SVGInstance } from "@instance/SVGInstance.js";
 export class VectorEffect extends SVGInstance {
 
   public vectorEffect(): string | null;
-  public vectorEffect(vectorEffect: "non-scaling-stroke" | "non-scaling-fill" | "non-scaling-stroke-and-fill"): this;
-  public vectorEffect(vectorEffect?: "non-scaling-stroke" | "non-scaling-fill" | "non-scaling-stroke-and-fill"): string | this | null {
+  public vectorEffect(vectorEffect: "non-scaling-stroke" | "non-scaling-size" | "non-rotation" | "fixed-position"): this;
+  public vectorEffect(vectorEffect?: "non-scaling-stroke" | "non-scaling-size" | "non-rotation" | "fixed-position"): string | this | null {
     if(typeof vectorEffect === "undefined"){
       const vectorEffect = this.attr("vector-effect");
       return typeof vectorEffect === "string" ? vectorEffect : null;
