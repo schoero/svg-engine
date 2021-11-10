a1bc5301151e68b54373d3d245bb19aa2581b89a
Roger Schönbächler <schoero@gmx.ch>
2021-11-10 09:03:01 +0100

    [classList] add support for  setting/removing multiple classes

--- src/node/dom/classes/classList.ts
+++ src/node/dom/classes/classList.ts
@@ -18,14 +18,22 @@ export default class ClassList extends Array {
   }
 
 
-  public add(className: string): void {
-    this.unshift(className);
+  public add(...classNames: Array<string>): void {
+    for(const className of classNames){
+      if(!this.includes(className)){
+        this.unshift(className);
+      }
+    }
     this.__updateAttribute();
   }
 
 
-  public remove(className: string): void {
-    this.splice(this.indexOf(className), 1);
+  public remove(...classNames: Array<string>): void {
+    for(const className of classNames){
+      if(this.includes(className)){
+        this._splice(className);
+      }
+    }
     this.__updateAttribute();
   }
 
@@ -47,13 +55,30 @@ export default class ClassList extends Array {
   }
 
 
+  private _splice(element: string): Array<string> {
+    let index = this.indexOf(element);
+    if(index > -1){
+      const newLength = this.length - 1;
+      while(index < newLength){
+        this[index] = this[index + 1];
+        ++index;
+      }
+      this.length = newLength;
+      return [element];
+    }
+    return [];
+  }
+
+
   public get value(): string {
     return this.join(" ");
   }
 
 
   public set value(value: string) {
-    this.splice(0, this.length);
+    while(this.length > 0){
+      this.pop();
+    }
     this.push(...value.split(" "));
     this.__updateAttribute();
   }
